64378a3fc06dbee12d0730c560a4c0693c3fb6e8
ConteMan <boxiaozhi.bolu@gmail.com>
2021-02-10 02:45:00 +0800

    :sparkles:  feat: 引入 Antd 等;
    - 引入 Antd;
    - 组件使用;

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Card } from 'antd';
+import Hello from './components/Hello/HelloFunction';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        style={{ width: '50%', minWidth: 300 }}
+      >
+        <p>React Demo</p>
+      </Card>
+      <Hello
+        style={{ marginTop: 50 }}
+        name="TypeScript"
+        enthusiasmLevel={10}
+      />
     </div>
   );
 }
